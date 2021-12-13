a7cd18a47454c82ad3683c37771abdc59b5b86b7
Dmitry Bogodyazh <dima3911784@hotmail.com>
2021-12-13 12:22:43 +0300

    feat: move weather controller logic to separate file

---
+++ controllers/weather.controller.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+import { requestOptions } from "../utils/requestOptions.js";
+
+export class WeatherController {
+  static async getWeather(req, res) {
+    await axios
+      .request(requestOptions(req.query.cityName))
+      .then((response) => {
+        if (response.status === 200) {
+          res.status(200).json(response.data);
+        } else {
+          res.status(response.status).json(response.data);
+        }
+      })
+      .catch((error) => {
+        res.status(error.response.status).send(error.response.statusText);
+      });
+  }
+}
