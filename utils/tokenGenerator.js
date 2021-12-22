08c9e17cbd56721443157e1e8406c463589808d4
Dmitry Bogodyazh <dima3911784@hotmail.com>
2021-12-22 08:38:00 +0300

    refactor: change tokenGenerator to functions (not const)

--- utils/tokenGenerator.js
+++ utils/tokenGenerator.js
@@ -1,17 +1,22 @@
 import jwt from "jsonwebtoken";
+import config from "config";
+
 import { User } from "../model/user.js";
 import { connect } from "../config/database.js";
 
-const { TOKEN_KEY_ACCESS, TOKEN_KEY_REFRESH } = process.env;
+const tokenKeyAccess = config.get("token.tokenKeyAccess");
+const tokenKeyRefresh = config.get("token.tokenKeyRefresh");
 
 connect();
 
 const user = await User.findOne({ id: "61b33711da2eb61149cb521b" });
 
-export const token = jwt.sign({ user_id: user._id }, TOKEN_KEY_ACCESS, {
-  expiresIn: "5m",
-});
+export const token = () =>
+  jwt.sign({ user_id: user._id }, tokenKeyAccess, {
+    expiresIn: "5m",
+  });
 
-export const refreshToken = jwt.sign({ user_id: user._id }, TOKEN_KEY_REFRESH, {
-  expiresIn: "30d",
-});
+export const refreshToken = () =>
+  jwt.sign({ user_id: user._id }, tokenKeyRefresh, {
+    expiresIn: "30d",
+  });
