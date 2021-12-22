96a405ebf6494bf116ff6ba89dd8fddd07f4de69
Dmitry Bogodyazh <dima3911784@hotmail.com>
2021-12-22 08:30:52 +0300

    refactor: change strings to const from config

--- utils/checkCookie.js
+++ utils/checkCookie.js
@@ -1,10 +1,15 @@
+import config from "config";
+
+const weatherAccessCookie = config.get("cookie.weatherAccessCookie");
+const weatherRefreshCookie = config.get("cookie.weatherRefreshCookie");
+
 export const checkCookie = (cookie) => {
   const cookieArr = cookie?.split("; ");
   const accessTokenFromCookie = cookieArr
-    ?.find((item) => item.split("=")[0] === "weather_access")
+    ?.find((item) => item.split("=")[0] === weatherAccessCookie)
     ?.split("=")[1];
   const refreshTokenFromCookie = cookieArr
-    ?.find((item) => item.split("=")[0] === "weather_refresh")
+    ?.find((item) => item.split("=")[0] === weatherRefreshCookie)
     ?.split("=")[1];
   return [accessTokenFromCookie, refreshTokenFromCookie];
 };
